aded584267c0cef0075754266775ac3a2826be91
Chandler Cunningham <59713235+ChandlerCunningham@users.noreply.github.com>
2020-01-09 19:51:15 -0600

    Create AllCopiesrentedOutException.java

---
+++ AllCopiesrentedOutException.java
@@ -0,0 +1,17 @@
+/**
+ * Exception is thrown if a requested movie has no video copy left in the store.
+ *
+ */
+public class AllCopiesRentedOutException extends Exception
+{
+	public AllCopiesRentedOutException()
+	{
+		super();
+	}
+	  
+	public AllCopiesRentedOutException(String msg)
+	{
+	    super(msg);
+	}
+
+}
